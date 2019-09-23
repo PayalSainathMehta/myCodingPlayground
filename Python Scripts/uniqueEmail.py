d0b59a0bb42272545aa21403c5133a4f3894b823
Payal Mehta <payal.mehta@stonybrook.edu>
2019-09-23 00:04:12 -0400

    Unique emails corrected

--- Python Scripts/uniqueEmail.py
+++ Python Scripts/uniqueEmail.py
@@ -1,16 +1,17 @@
 def numUniqueEmails(emails):
-	stack = []
-	for email in emails:
-                parts = email.split('@')
-                new_part = parts[0].replace('.','')
-		new_part2 = new_part[0:new_part.find('+')]
-                res = new_part2 + parts[1]
-                if res not in stack:
-                        stack.append(res)
-                else continue
+    stack = []
+    for email in emails:
+        parts = email.split('@')
+        new_part = parts[0].replace('.','')
+        if '+' in new_part:
+            new_part2 = new_part[0:new_part.find('+')]
+        else:
+            new_part2 = new_part
+        res = new_part2 + '@' + parts[1]
+        if res not in stack:
+            stack.append(res)
 
-        return len(stack)
+    return len(stack)
 
 
-numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
-)
+print(numUniqueEmails(["test.email+alex@leetcode.com","test.email.leet+alex@code.com"]))
